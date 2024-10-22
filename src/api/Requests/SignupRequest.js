73581b5c14923a24d32ec00371c63b0ba0b9c2b4
ahonkazi <aohinuzzaman420@gmail.com>
2024-10-22 08:53:11 +0600

    request changed

--- src/api/Requests/SignupRequest.js
+++ src/api/Requests/SignupRequest.js
@@ -2,8 +2,8 @@ const { check } = require("express-validator");
 const Request = require("../Middlewares/Request");
 
 const SignupRequest = [
-  check("email").isEmail().withMessage("Valid email is required"),
   check("fullName").isString().withMessage("Full name is required"),
+  check("email").isEmail().withMessage("Valid email is required"),
   check("password").isString().withMessage("Password is required"),
   check("verificationCode")
     .isNumeric()
