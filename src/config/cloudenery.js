bd7560586ab28dcb925f1b2e819425a63211197f
ahonkazi <aohinuzzaman420@gmail.com>
2024-10-26 10:27:21 +0600

    done

--- src/config/cloudenery.js
+++ src/config/cloudenery.js
@@ -6,7 +6,7 @@ const cloudinary = require("cloudinary").v2;
 // Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: 317728426582323,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
